03197a96d652082d8457861a3ab8d4897f1398e1
cpintoval <cpintovalderrama@gmail.com>
2015-08-31 21:33:33 +0000

    Task 1: Main menu and user input

--- contact_list.rb
+++ contact_list.rb
@@ -3,3 +3,27 @@ require_relative 'contact_database'
 
 # TODO: Implement command line interaction
 # This should be the only file where you use puts and gets
+class Application
+
+  def initialize
+
+  end
+
+  # Main method to 
+  def run
+    arg = ARGV[0]
+    case arg
+    when 'help'
+      puts "Here is a list of available commands:"
+      puts "    new  - Create a new contact"
+      puts "    list - List all contacts"
+      puts "    show - Show a contact"
+      puts "    find - Find a contact"
+    end
+  end
+
+end
+
+# Creation of an Application instance and running it
+app = Application.new
+app.run
